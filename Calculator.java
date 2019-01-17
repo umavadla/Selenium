2fc1eb659dfbdebe375cfbe2e56c39f9ab45b63c
umavadla <40799985+umavadla@users.noreply.github.com>
2019-01-17 10:27:40 -0500

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+	package com.pack1;
+	
+	public class Calculator 
+	{
+		private int privateVariable;
+		protected int protectedVariable;
+		int defaultVariable;
+		public int publicVariable;
+	
+		
+		private void privateMethod()
+		{
+		}
+		protected void protectedMethod()
+		{
+		}
+		void defaultMethod()
+		{
+		}
+		public void publicMethod()
+		{
+		}
+	}
